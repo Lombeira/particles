290ef5286b2b80bd13b217d0d7cf8ab7e4783e69
Lombeira <glomenha@gmail.com>
2021-07-21 10:04:26 -0300

    Base Particles

---
+++ script.js
@@ -0,0 +1,74 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particle = [];
+
+//handle mouse
+
+const mouse = {
+  x: null,
+  y: null,
+  raidus: 150,
+};
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  // console.log(mouse.x, mouse.y);
+});
+
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 20, 60);
+// ctx.strokeStyle = "white";
+// ctx.strokeRect(0, 0, 100, 100);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 2;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 30 + 1;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < 100) {
+      this.size = 30;
+    } else {
+      this.size = 2;
+    }
+  }
+}
+
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 1000; i += 1) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    particleArray.push(new Particle(x, y));
+  }
+}
+init();
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  requestAnimationFrame(animate);
+}
+animate();
