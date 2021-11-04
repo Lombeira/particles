cdd2b8c333cf8847115e92da0b5d4a33891ddab5
Lombeira <glomenha@gmail.com>
2021-11-04 09:58:08 -0300

    particles

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particle = [];
+let adjustX = -15;
+let adjustY = -8;
 
 //handle mouse
 
@@ -12,18 +14,18 @@ const mouse = {
   radius: 50,
 };
 
-window.addEventListener("mousemove", function (event) {
+window.addEventListener('mousemove', function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
   // console.log(mouse.x, mouse.y);
 });
 
-ctx.fillStyle = "white";
-ctx.font = "30px Verdana";
-ctx.fillText("A", 20, 60);
+ctx.fillStyle = 'white';
+ctx.font = '20px Sans-serif';
+ctx.fillText('Lombeira', 15, 25);
 // ctx.strokeStyle = "white";
 // ctx.strokeRect(0, 0, 100, 100);
-const data = ctx.getImageData(0, 0, 100, 100);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 class Particle {
   constructor(x, y) {
@@ -32,10 +34,10 @@ class Particle {
     this.size = 2;
     this.baseX = this.x;
     this.baseY = this.y;
-    this.density = Math.random() * 30 + 1;
+    this.density = Math.random() * 40 + 5;
   }
   draw() {
-    ctx.fillStyle = "white";
+    ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.closePath();
@@ -55,18 +57,37 @@ class Particle {
       this.x -= directionX;
       this.y -= directionY;
     } else {
-      this.size = 2;
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseX) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
     }
   }
 }
 
 function init() {
   particleArray = [];
-  for (let i = 0; i < 1000; i += 1) {
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * canvas.height;
-    particleArray.push(new Particle(x, y));
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let positionX = x + adjustX;
+        let positionY = y + adjustY;
+        particleArray.push(new Particle(positionX * 8, positionY * 8));
+      }
+    }
   }
+  //Loop for Random particles around the canvas
+  // for (let i = 0; i < 1000; i += 1) {
+  //   let x = Math.random() * canvas.width;
+  //   let y = Math.random() * canvas.height;
+  //   particleArray.push(new Particle(x, y));
+  // }
 }
 init();
 
@@ -76,6 +97,30 @@ function animate() {
     particleArray[i].draw();
     particleArray[i].update();
   }
+  // connect();
   requestAnimationFrame(animate);
 }
 animate();
+
+function connect() {
+  let opacityValue = 1;
+  for (let a = 0; a < particleArray.length; a++) {
+    for (let b = a; b < particleArray.length; b++) {
+      // let dx = mouse.x - this.x;
+      // let dy = mouse.y - this.y;
+      // let distance = Math.sqrt(dx * dx + dy * dy);
+      let dx = particleArray[a].x - particleArray[b].x;
+      let dy = particleArray[a].y - particleArray[b].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      opacityValue = 1 - distance / 16;
+      ctx.strokeStyle = 'rgba(255, 255, 255,' + opacityValue + ')';
+      if (distance < 16) {
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+const message = 'i like dogs';
+const message2 = 'teste';
+
+var message3 = 3;
